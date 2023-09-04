68da5258de1b849ae84683633c22bbbab842563e
martin-luik <martinluik@hotmail.com>
2023-09-04 16:38:46 +0300

    Added cors config

---
+++ src/main/java/com/helmes/app/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.helmes.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("POST", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Methods",
+                        "Access-Control-Allow-Headers",
+                        "Access-Control-Request-Headers",
+                        "Access-Control-Request-Method");
+    }
+}
