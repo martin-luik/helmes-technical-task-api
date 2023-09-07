29ce5ef6cdc8bc721f78fe9f049ad48000f8559e
martin-luik <martinluik@hotmail.com>
2023-09-07 13:46:36 +0300

    Updated cors config

--- src/main/java/com/helmes/app/config/CorsConfig.java
+++ src/main/java/com/helmes/app/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://localhost:3000")
                 .allowedMethods("POST", "PATCH", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Access-Control-Allow-Origin",
